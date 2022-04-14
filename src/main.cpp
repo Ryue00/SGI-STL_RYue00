140eafd699581de6974558e4862088f9378301b1
Yry 叶荣跃 <xmyry@intretech.com>
2022-04-14 21:58:44 +0800

    1.增加:STL空间配置器allocator分析以及笔记;

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+using namespace std;
+
+void before() __attribute__((constructor));
+void after() __attribute__((destructor));
+void before(){
+    printf("this is function %s\n",__func__);
+    return;
+}
+
+void after(){
+    printf("this is function %s\n",__func__);
+    return;
+}
+
+typedef void *function ();
+
+int main(int argc, char** argv)
+{
+     std::cout << "hello world!" << std::endl;
+     system("pause");
+    return 0;
+}
+
+// 输出结果
+// this is function before
+// this is function main
+// this is function after
+
+
